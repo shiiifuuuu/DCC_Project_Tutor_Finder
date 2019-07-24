8249a9fcaefd3507b9532c3e38b1b85d19c91179
shiiifuuuu <mdshahadat.hossain7@gmail.com>
2019-07-25 04:51:33 +0600

    4.51

---
+++ app/src/main/java/com/teamocta/dcc_project/Login.java
@@ -0,0 +1,97 @@
+package com.teamocta.dcc_project;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Login extends AppCompatActivity {
+
+    //V A R I A B L E S
+    private EditText etEmail;
+    private EditText etPassword;
+
+    private String userEmail, userPassword;
+
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        init();
+
+    }
+
+    //I N I T I A L I Z I N G    A L L    I D S
+    private void init() {
+        etEmail=findViewById(R.id.etEmail);
+        etPassword=findViewById(R.id.etPassword);
+
+        firebaseAuth=FirebaseAuth.getInstance();
+    }
+
+    //O N     C L I C K
+    public void btnLoginClicked(View view) {
+        userEmail=etEmail.getText().toString();
+        userPassword=etPassword.getText().toString();
+
+        if(userEmail.equals("") || userPassword.equals("")){
+            toastMessageShort("Fields Empty");
+        }
+        else{
+            signInUser(userEmail, userPassword);
+        }
+    }
+
+    public void tvForgetPassClicked(View view) {
+        Intent intent = new Intent(Login.this, ForgetPass.class);
+        startActivity(intent);
+    }
+
+    public void tvSignupClicked(View view) {
+        Intent intent = new Intent(Login.this, Signup.class);
+        startActivity(intent);
+        finish();
+    }
+
+    //S I G N    I N    M E T H O D
+    private void signInUser(String userEmail, String userPassword) {
+        firebaseAuth.signInWithEmailAndPassword(userEmail, userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(!task.isSuccessful()){
+                    toastMessageShort("Sign In ERROR!!");
+                    toastMessageLong("Check your email or password again.");
+                }else{
+                    if(firebaseAuth.getCurrentUser().isEmailVerified()){
+                        Intent intent = new Intent(Login.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }else{
+                        toastMessageLong("Please verify your email first");
+                    }
+                }
+            }
+        });
+    }
+
+    //T O A S T    M E S S A G E
+    private void toastMessageShort(String msg) {
+        Toast.makeText(Login.this, msg, Toast.LENGTH_SHORT).show();
+    }
+    private void toastMessageLong(String msg){
+        Toast.makeText(Login.this, msg, Toast.LENGTH_LONG).show();
+    }
+
+}
--- app/src/main/java/com/teamocta/dcc_project/SplashScreen.java
+++ app/src/main/java/com/teamocta/dcc_project/SplashScreen.java
@@ -2,13 +2,33 @@ package com.teamocta.dcc_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        init();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 6000);
+
+    }
+
+    private void init() {
+        handler = new Handler();
     }
 }
