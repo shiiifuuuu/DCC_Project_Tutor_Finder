1a3814bfe72b71d8675268b9116af24b6508947f
shiiifuuuu <mdshahadat.hossain7@gmail.com>
2019-07-25 04:53:40 +0600

    login done forget password done

---
+++ app/src/main/java/com/teamocta/dcc_project/Signup.java
@@ -0,0 +1,19 @@
+package com.teamocta.dcc_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        
+        init();
+    }
+
+    private void init() {
+    }
+}
