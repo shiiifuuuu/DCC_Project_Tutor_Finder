38a775453ab7275dda4ce29ae6e63a014d8efc74
Shahadat <mdshahadat.hossain7@gmail.com>
2019-08-01 09:03:59 +0600

    login and signup working fine

--- app/src/main/java/com/teamocta/dcc_project/LoginActivity.java
+++ app/src/main/java/com/teamocta/dcc_project/LoginActivity.java
@@ -1,7 +1,6 @@
 package com.teamocta.dcc_project;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
@@ -15,7 +14,6 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
