9b28e3045ca11da447723f3b121a977a144ce992
Shahadat <mdshahadat.hossain7@gmail.com>
2019-08-04 19:34:24 +0600

    added activities

---
+++ app/src/main/java/com/teamocta/dcc_project/TutorProfileActivity.java
@@ -0,0 +1,14 @@
+package com.teamocta.dcc_project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TutorProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutor_profile);
+    }
+}
